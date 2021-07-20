3a26f617ace8cb96eda927e90984be9e0dc15a53
takeru-doi-0210 <takeru_doi@acceluniverse.com>
2021-07-20 17:45:34 +0900

    otoshidama miss

---
+++ otosidama.js
@@ -0,0 +1,10 @@
+const billCounts = {
+  yen10000Count: 0,
+  yen5000Count: 0,
+  yen1000Count: 0,
+}
+
+billCounts.yen10000Count = billCounts.yen10000Count + 1
+
+
+console.log(billCounts.yen10000Count)
